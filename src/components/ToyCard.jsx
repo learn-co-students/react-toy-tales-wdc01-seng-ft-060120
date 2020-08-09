48df937a793b8ba2c1f54caf025df161c1479377
mjester93 <m.jester.93@gmail.com>
2020-08-09 15:23:49 -0400

    Done.

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,14 +2,54 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      likes: this.props.toy.likes
+    }
+  }
+
+  addOneLike = () => {
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'accept': 'application/json',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        likes: parseInt(this.state.likes, 10) + 1
+      })
+    }
+
+    fetch(`http://localhost:3000/toys/${this.props.toy.id}`, options)
+    .then(response => response.json())
+    .then(toyData => this.setState({likes: toyData.likes}))
+    .catch(error => alert(error))
+  }
+
+  deleteToy = () => {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'accept': 'application/json',
+        'content-type': 'application/json'
+      }
+    }
+
+    fetch(`http://localhost:3000/toys/${this.props.toy.id}`, options)
+    .then(response => response.json())
+    .then(toyData => this.setState({likes: toyData.likes}))
+    .catch(error => alert(error))
+  }
+
   render() {
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{this.props.toy.name}</h2>
+        <img src={this.props.toy.image} alt={this.props.toy.name} className="toy-avatar" />
+        <p>{this.state.likes} Likes </p>
+        <button className="like-btn" onClick={() => this.addOneLike()}>Like {'<3'}</button>
+        <button className="del-btn" onClick={() => this.deleteToy()}>Donate to GoodWill</button>
       </div>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  addNewToy = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const image = event.target.image.value;
+
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        image: image,
+        likes: 0
+      })
+    }
+
+    fetch('http://localhost:3000/toys', options)
+    .then(response => response.json())
+    .then(toyData => this.props.updateToyStateAdd(toyData))
+    
+    event.target.reset();
+  }
+
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={(event) => this.addNewToy(event)}>
           <h3>Create a toy!</h3>
           <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
