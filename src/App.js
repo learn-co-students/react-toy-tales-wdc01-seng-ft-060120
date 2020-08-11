ee9706a44a32a63ce2aecf2bcbbc214677696cec
BartTheScrivner <dframe22@gmail.com>
2020-08-10 21:56:01 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
@@ -11,7 +10,18 @@ import data from './data'
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: [],
+    displayToys: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(toys => this.setState({
+      toys: toys,
+      displayToys: toys
+    }))
   }
 
   handleClick = () => {
@@ -27,16 +37,73 @@ class App extends React.Component{
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm sendForm={this.sendForm} />
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.displayToys} handleLike={this.handleLike} handleDelete={this.handleDelete}/>
       </>
     );
+  }
+
+  sendForm = (formData) => {
+    const postRequest = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({...formData, likes: 0 })
+    }
+    fetch("http://localhost:3000/toys", postRequest)
+    .then(resp => resp.json())
+    .then(toy => this.setState({
+      toys: [...this.state.toys, toy],
+      displayToys: [...this.state.displayToys, toy]
+    }))
+  }
+
+  handleLike = (toy) => {
+    const patchRequest = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({likes: toy.likes + 1 })
+    }
+    fetch(`http://localhost:3000/toys/${toy.id}`, patchRequest)
+    .then(resp => resp.json())
+    .then(likedToy => this.setState({
+      toys: this.state.toys.map(toy => toy.id === likedToy.id ? likedToy : toy),
+      displayToys: this.state.toys.map(toy => toy.id === likedToy.id ? likedToy : toy)
+    }))
+  }
+
+  handleDelete = (toy) => {
+    const deleteRequest = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    }
+    fetch(`http://localhost:3000/toys/${toy.id}`, deleteRequest)
+    .then(resp => resp.json())
+    .then(() => this.removeToy(toy))
+  }
+
+  removeToy = (oldToy) => {
+    
+    const toys = this.state.toys.filter(toy => toy.id !== oldToy.id)
+    const displayToys = this.state.displayToys.filter(toy => toy.id !== oldToy.id)
+    this.setState({
+      toys: toys,
+      displayToys: displayToys
+    })
   }
 
 }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 
 class ToyForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      image: ""
+    };
+    this.sendForm = this.props.sendForm;
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.sendForm(this.state);
+    this.setState({name: "", image: ""});
+  }
 
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form onSubmit={(e) => this.handleSubmit(e)} className="add-toy-form">
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input onChange={this.handleChange} type="text" name="name" placeholder="Enter a toy's name..." className="input-text" value={this.state.name} />
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input onChange={this.handleChange} type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text" value={this.state.image} />
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
